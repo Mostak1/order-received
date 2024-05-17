df09e39ec36111b1d3651b45192362acad5eefa5
Mostak1 <mostakidb@gmail.com>
2024-05-17 12:51:08 +0600

    update css file

--- routes/web.php
+++ routes/web.php
@@ -5,18 +5,17 @@ use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ReceivedController;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/home', function () {
+Route::middleware('auth')->group(function () {
+Route::get('/', function () {
     return view('home');
 });
-
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 Route::resource('orders', OrderController::class);
 Route::resource('receiveds', ReceivedController::class);
 Route::resource('products', ProductController::class);
-Route::middleware('auth')->group(function () {
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
